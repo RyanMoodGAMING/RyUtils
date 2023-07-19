c430f046e6501e76643bef8d415b5815381eb2e9
RyanMoodGAMING <ryan@ryanmood.com>
2023-07-19 17:02:27 +0100

    Add copyright

--- ryutils-bungee/src/main/java/me/ryanmood/ryutils/bungee/PluginMessages.java
+++ ryutils-bungee/src/main/java/me/ryanmood/ryutils/bungee/PluginMessages.java
@@ -12,6 +12,14 @@ import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+/*
+ * This software and its content is copyright of RyanMoodGAMING - © RyanMoodGAMING 2023. All rights reserved.
+ * Any redistribution or reproduction of part or all of the contents in any form is prohibited other than the following:
+ * you may print or download to a local hard disk extracts for your personal and non-commercial use only
+ * you may copy the content to individual third parties for their personal use, but only if you acknowledge the website as the source of the material
+ * You may not, except with our express written permission, distribute or commercially exploit the content. Nor may you transmit it or store it in any other website or other form of electronic retrieval system.
+ */
+
 public abstract class PluginMessages implements Listener {
 
     @Getter
--- ryutils-spigot/src/main/java/me/ryanmood/ryutils/spigot/PluginMessages.java
+++ ryutils-spigot/src/main/java/me/ryanmood/ryutils/spigot/PluginMessages.java
@@ -13,6 +13,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.util.UUID;
 
+/*
+ * This software and its content is copyright of RyanMoodGAMING - © RyanMoodGAMING 2023. All rights reserved.
+ * Any redistribution or reproduction of part or all of the contents in any form is prohibited other than the following:
+ * you may print or download to a local hard disk extracts for your personal and non-commercial use only
+ * you may copy the content to individual third parties for their personal use, but only if you acknowledge the website as the source of the material
+ * You may not, except with our express written permission, distribute or commercially exploit the content. Nor may you transmit it or store it in any other website or other form of electronic retrieval system.
+ */
+
 public abstract class PluginMessages implements PluginMessageListener {
 
     @Getter
