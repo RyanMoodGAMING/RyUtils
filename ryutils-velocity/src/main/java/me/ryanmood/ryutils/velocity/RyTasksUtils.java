da22af15b850aa2fa8d81ed1940e0b6b0e9477cc
RyanMoodGAMING <ryan@ryanmood.com>
2024-01-25 23:01:56 +0000

    Add javadocs stuff

--- ryutils-velocity/src/main/java/me/ryanmood/ryutils/velocity/RyTasksUtils.java
+++ ryutils-velocity/src/main/java/me/ryanmood/ryutils/velocity/RyTasksUtils.java
@@ -20,26 +20,72 @@ public class RyTasksUtils {
     @Setter
     private static ProxyServer server;
 
+    /**
+     * Create a task that runs within a scheduler.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     */
     public static void run(@NotNull Plugin plugin, Runnable callable) {
         runAsync(plugin, callable);
     }
 
+    /**
+     * Create a task that runs within an async scheduler.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     */
     public static void runAsync(@NotNull Plugin plugin, Runnable callable) {
         server.getScheduler().buildTask(plugin, callable).schedule();
     }
 
+    /**
+     * Create a task that runs within a scheduler after a certain amount of time.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     * @param delay    The delay before the runnable runs.
+     * @param timeUnit The time unit.
+     */
     public static void runLater(@NotNull Plugin plugin, Runnable callable, long delay, TimeUnit timeUnit) {
         runAsyncLater(plugin, callable, delay, timeUnit);
     }
 
+    /**
+     * Create a task that runs within an async scheduler after a certain amount of time.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     * @param delay    The delay before the runnable runs.
+     * @param timeUnit The time unit.
+     */
     public static void runAsyncLater(@NotNull Plugin plugin, Runnable callable, long delay, TimeUnit timeUnit) {
         server.getScheduler().buildTask(plugin, callable).delay(delay, timeUnit).schedule();
     }
 
+    /**
+     * Create a task that runs on a timer within a scheduler after a certain amount of time.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     * @param delay    The delay before the runnable runs.
+     * @param interval The interval before the runnable runs again.
+     * @param timeUnit The time unit.
+     */
     public static void runTimer(@NotNull Plugin plugin, Runnable callable, long delay, long interval, TimeUnit timeUnit) {
         runAsyncTimer(plugin, callable, delay, interval, timeUnit);
     }
 
+    /**
+     * Create a task that runs on a timer within an async scheduler after a certain amount of time.
+     *
+     * @param plugin   The instance of the plugin you are using.
+     * @param callable The Runnable code you would like to happen.
+     * @param delay    The delay before the runnable runs.
+     * @param interval The interval before the runnable runs again.
+     * @param timeUnit The time unit.
+     */
     public static void runAsyncTimer(@NotNull Plugin plugin, Runnable callable, long delay, long interval, TimeUnit timeUnit) {
         server.getScheduler().buildTask(plugin, callable).delay(delay, timeUnit).repeat(interval, timeUnit).schedule();
     }
